6973b593f4bd1cc230459974f38e8ec381b8d06c
valentinusilalahi <valentinus.silalahi@gmail.com>
2019-03-22 22:31:37 +0700

    first commit

---
+++ src/main/java/com/silalahi/valentinus/sample/RestfullSampleApplication.java
@@ -0,0 +1,13 @@
+package com.silalahi.valentinus.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfullSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfullSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/silalahi/valentinus/sample/RestfullSampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.silalahi.valentinus.sample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestfullSampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
